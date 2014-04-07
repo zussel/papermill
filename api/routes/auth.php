a75d7bb57ee0959adbb825eec560232e65b7959e
zussel <sascha.kuehl@gmx.net>
2014-04-07 22:54:02 +0200

    introduced auth paths

---
+++ api/routes/auth.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sascha
+ * Date: 07.04.14
+ * Time: 21:31
+ */
+
+$app->group('/auth', function () use ($app) {
+
+    /*
+     * login a user
+     */
+    $app->post('/login', function () use ($app) {
+    });
+});
