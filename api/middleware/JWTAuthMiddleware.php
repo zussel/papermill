3330975b831e21073982af3f4d5b84e837f351dc
zussel <sascha.kuehl@gmx.net>
2014-04-07 22:54:27 +0200

    added JWT authentication middleware

---
+++ api/middleware/JWTAuthMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sascha
+ * Date: 07.04.14
+ * Time: 22:02
+ */
+
+class JWTAuthMiddleware extends \Slim\Middleware
+{
+    public function call()
+    {
+        /*
+         * get resource uri
+         */
+        $path = $this->app->request->getResourceUri();
+        /*
+         * get auth header
+         */
+        $auth = $this->app->request->headers->get('Authorization');
+
+        if ($path == "/auth/login" && $auth != null) {
+            // TODO: handle login when already logged in
+        } else if ($path != "/auth/login" && $auth == null) {
+            // not authenticated: return 401
+            $this->app->response()->status(401);
+        } else {
+            // TODO: check auth
+            $this->next->call();
+        }
+    }
+}
