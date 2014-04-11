bbf728f678cf62a90c81b465c76665a2d6fb77f8
Sascha Kuehl <sascha.kuehl@identpro.de>
2014-04-11 15:43:06 +0200

    added submit function

--- app/modules/login/controller/LoginCtrl.js
+++ app/modules/login/controller/LoginCtrl.js
@@ -1,4 +1,11 @@
 var app = angular.module('papermill');
 
-app.controller('LoginCtrl', ['$scope', function($scope) {
+app.controller('LoginCtrl', ['$scope', 'AuthService', function($scope, AuthService) {
+
+    $scope.credentials = {};
+
+    $scope.login = function() {
+        console.log($scope.credentials);
+        AuthService.login($scope.credentials);
+    }
 }]);
