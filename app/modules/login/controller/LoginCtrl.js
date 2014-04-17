5a0b5b699d60ed31641f4cee3dd9cd24c6c7f2af
Sascha Kuehl <sascha.kuehl@identpro.de>
2014-04-17 16:25:58 +0200

    login progress

--- app/modules/login/controller/LoginCtrl.js
+++ app/modules/login/controller/LoginCtrl.js
@@ -1,11 +1,15 @@
 var app = angular.module('papermill');
 
-app.controller('LoginCtrl', ['$scope', 'AuthService', function($scope, AuthService) {
+app.controller('LoginCtrl', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService) {
 
     $scope.credentials = {};
 
     $scope.login = function() {
         console.log($scope.credentials);
         AuthService.login($scope.credentials);
-    }
+    };
+
+    $scope.register = function() {
+    	$location.path('/');
+    };
 }]);
