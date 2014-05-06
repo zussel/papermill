bf2c0b2fe0d2d7acecfdeaef2aa2d3df61a6f343
zussel <sascha.kuehl@gmx.net>
2014-05-06 14:17:30 +0200

    added regexp to parse bearer token

--- api/middleware/JWTAuthMiddleware.php
+++ api/middleware/JWTAuthMiddleware.php
@@ -8,7 +8,13 @@
 
 class JWTAuthMiddleware extends \Slim\Middleware
 {
-    var $special_paths = array("/auth/login", "/auth/signin", "/setup");
+    /**
+	 * Regular expression to extract token from the HTTP headers
+	 * @var string
+	 */
+	protected $bearerTokenRegEx = '#Bearer\s(\S+)#';
+	
+	var $special_paths = array("/auth/login", "/auth/signin", "/setup");
 
     public function call()
     {
