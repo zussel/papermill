17c8bcee4459d83b70cc62e8c1faf51ec174de78
Sascha Kuehl <sascha.kuehl@identpro.de>
2014-05-13 17:12:03 +0200

    refactored autor route to profile

--- api/routes/profiles.php
+++ api/routes/profiles.php
@@ -6,33 +6,33 @@
  * Time: 3:17 PM
  */
 
-$app->group('/author', function () use ($app) {
+$app->group('/profile', function () use ($app) {
 
     /*
-     * get all authors
+     * get all profiles
      */
     $app->get('', function () use ($app) {
         $app->response()->header("Content-Type", "application/json");
-        $authors = Model::factory('Author')
+        $profiles = Model::factory('Profile')
             ->order_by_asc("last_name")
             ->find_many();
 
-        $authors = array_map(function($a) {
+        $profiles = array_map(function($a) {
                 $arr = $a->as_array();
                 $arr["first_name"] = utf8_encode($arr["first_name"]);
                 $arr["last_name"] = utf8_encode($arr["last_name"]);
                 return $arr; },
-            $authors);
-        echo json_encode($authors);
+            $profiles);
+        echo json_encode($profiles);
     });
 
     /*
-     * get author by id
+     * get profile by id
      */
     $app->get('/:id', function ($id) use ($app) {
-        $author = Model::factory('Author')->find_one($id);
-        if ($author != null) {
-            $arr = $author->as_array();
+        $profile = Model::factory('Profile')->find_one($id);
+        if ($profile != null) {
+            $arr = $profile->as_array();
             $arr["first_name"] = utf8_encode($arr["first_name"]);
             $arr["last_name"] = utf8_encode($arr["last_name"]);
             echo json_encode($arr);
@@ -42,27 +42,27 @@ $app->group('/author', function () use ($app) {
     });
 
     $app->post('', function () use ($app) {
-        $author = Model::factory('Author')->create();
-        $author->save();
-        $arr = $author->as_array();
+        $profile = Model::factory('Profile')->create();
+        $profile->save();
+        $arr = $profile->as_array();
         $arr["first_name"] = utf8_encode($arr["first_name"]);
         $arr["last_name"] = utf8_encode($arr["last_name"]);
         echo json_encode($arr);
     });
 
     $app->put('/:id', function ($id) use ($app) {
-        $author = Model::factory('Author')->find_one($id);
-        $author->save();
-        $arr = $author->as_array();
+        $profile = Model::factory('Profile')->find_one($id);
+        $profile->save();
+        $arr = $profile->as_array();
         $arr["first_name"] = utf8_encode($arr["first_name"]);
         $arr["last_name"] = utf8_encode($arr["last_name"]);
         echo json_encode($arr);
     });
 
     $app->delete('/:id', function ($id) use ($app) {
-        $author = Model::factory('Author')->find_one($id);
-        if ($author != null) {
-            $author->delete();
+        $profile = Model::factory('Profile')->find_one($id);
+        if ($profile != null) {
+            $profile->delete();
             $app->response->setStatus(204);
         } else {
             $app->response->setStatus(404);
